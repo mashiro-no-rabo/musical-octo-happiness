27f0ed52d3de052812a1ee65f27491b8db32402c
AquarHEAD Lou <aquarhead@ela.build>
2018-07-16 16:34:52 +0000

    Cleanup logging

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,12 @@
 use Mix.Config
+
+config :lager,
+  error_logger_redirect: true,
+  colored: true,
+  handlers: [
+    lager_console_backend: [level: :notice]
+  ]
+
+config :logger,
+  level: :error,
+  truncate: 4096
